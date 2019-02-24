c326002e021ac6b085f619ced7ab903d6c3c2672
Vahe Sasunts <vahe@Vahes-iMac.local>
2019-02-24 11:39:13 +0000

    Basic working code, handles requests sends to connection

--- src/server.py
+++ src/server.py
@@ -1,18 +1,115 @@
 import socket
 import sys
-from random import randint
+import threading
+import os
+
+MAX_CONNECTION = 50
+BUFFER = 5120
+HOST = '127.0.0.1'
+port_listen = ""
+
 
 def Main():
-    #port generation between numbers 5000 and 30000
-    port = randint(5000, 30000)
+    global port_listen
+    while True:
+        userIn = raw_input("|C - Connection | S - Settings | E - Exit|\n")
 
-    #local host
-    host = '127.0.0.1'
+        if(userIn is "c"):
+            while True:
+                try:
+                    port_listen = int(raw_input("Enter proxy port: "))
+                    run()
+                except socket.error:
+                    print("Try again in port in use")
+                    pass
+        elif(userIn is "e"):
+            print("Exiting")
+            sys.exit()
 
-    #TCP Socket creation
-    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    serverSocket.bind((host, port))
-    print("Server started on port:" + str(port) + "\n")
+
+def run():
+    global port_listen
+    print(port_listen)
+    try:
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.bind(('', int(port_listen)))
+        server.listen(MAX_CONNECTION)
+        print("Server Started on Port: " + str(port_listen))
+        print("Running proxy")
+    except Exception:
+        print("Unable to start server")
+        sys.exit()
+
+    while True:
+        try:
+            conn, addr = server.accept()
+            request = conn.recv(BUFFER)
+            pyThread = threading.Thread(target = handler, args=(conn, request))
+            pyThread.daemon = True
+            pyThread.start()
+        except KeyboardInterrupt:
+            server.close()
+            print("Exiting")
+            sys.exit()
+
+    server.close()
+
+
+def handler(conn, request):
+
+
+    try:
+        first_line = request.split('\n')[0]
+        url = first_line.split(' ')[1]
+        http_pos = url.find("://")
+
+        if(http_pos == -1):
+            temp = url
+        else:
+            temp = url[(http_pos+3):]
+
+
+        port_pos = temp.find(":")
+        webserver_pos = temp.find("/")
+        if webserver_pos == -1:
+            webserver_pos = len(temp)
+        webserver = ""
+        port = -1
+        if (port_pos==-1 or webserver_pos <port_pos):
+            port = 80
+            webserver = temp[:webserver_pos]
+        else:
+            port = int((temp[(port_pos+1):])[:webserver_pos-port_pos-1])
+            webserver = temp[:port_pos]
+
+        proxy_server(webserver, port, conn,request)
+    except Exception, e:
+        pass
+
+
+
+def proxy_server(webserver, port, conn, request):
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((webserver, port))
+        s.send(request)
+
+        while True:
+            reply = s.recv(BUFFER)
+
+            if(len(reply)>0):
+                conn.send(reply)
+                print("Request Handled : " + webserver)
+            else:
+                break
+
+            s.close()
+            conn.close()
+    except socket.error:
+        s.close()
+        conn.close()
+        sys.exit()
+
 
 
 
